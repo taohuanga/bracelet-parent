3ee223980234bc35f1c17b09018a375c28805c91
lishiyou <lishiyou>
2019-09-19 14:20:12 +0800

    取消日志上传 版本号增加

--- app/src/main/java/os/bracelets/parents/app/main/MainActivity.java
+++ app/src/main/java/os/bracelets/parents/app/main/MainActivity.java
@@ -307,8 +307,8 @@ public class MainActivity extends MVPBaseActivity<MainContract.Presenter> implem
             tvConnect.setText("已连接");
             onResume();
             LocalDeviceEntity entity = MyApplication.getInstance().getDeviceEntity();
-            uploadLog("设备" + (entity == null ? "--" :
-                    entity.getAddress().replace(":", "").toUpperCase()) + "连接成功！");
+//            uploadLog("设备" + (entity == null ? "--" :
+//                    entity.getAddress().replace(":", "").toUpperCase()) + "连接成功！");
         }
         //设备失去连接
         if (event.getAction() == AppConfig.MSG_DEVICE_DISCONNECT) {
--- app/src/main/java/os/bracelets/parents/service/AppService.java
+++ app/src/main/java/os/bracelets/parents/service/AppService.java
@@ -160,7 +160,7 @@ public class AppService extends Service implements DataSendCallback, SensorEvent
         @Override
         public void onFinish() {
             timer.start();
-            uploadLog("服务后台运行中，正常计时....,设备连接状态" + MyApplication.getInstance().isBleConnect());
+//            uploadLog("服务后台运行中，正常计时....,设备连接状态" + MyApplication.getInstance().isBleConnect());
             //计时结束 分发数据
             EventBus.getDefault().post(new MsgEvent<>(AppConfig.MSG_STEP_COUNT, CURRENT_STEP));
             uploadStepNum();
@@ -337,23 +337,23 @@ public class AppService extends Service implements DataSendCallback, SensorEvent
     }
 
     private void uploadData(final String data) {
-        uploadLog("设备数据上传中...");
+//        uploadLog("设备数据上传中...");
         ApiRequest.uploadBleData(data, new HttpSubscriber() {
             @Override
             public void onError(Throwable e) {
                 super.onError(e);
                 fileUtils.writeTxtToFile(data, "test6Sensor" + formatter.format(System.currentTimeMillis()) + ".csv");
-                uploadLog("设备数据上传失败，已缓存到本地转为定时任务");
+//                uploadLog("设备数据上传失败，已缓存到本地转为定时任务");
             }
 
             @Override
             public void onNext(HttpResult result) {
                 super.onNext(result);
                 if (result.code.equals(AppConfig.SUCCESS)) {
-                    uploadLog("设备数据上传成功...");
+//                    uploadLog("设备数据上传成功...");
                 } else {
                     fileUtils.writeTxtToFile(data, "test6Sensor" + formatter.format(System.currentTimeMillis()) + ".csv");
-                    uploadLog(System.currentTimeMillis() + "设备数据上传失败，已缓存到本地转为定时任务");
+//                    uploadLog(System.currentTimeMillis() + "设备数据上传失败，已缓存到本地转为定时任务");
                 }
             }
         });
@@ -364,21 +364,21 @@ public class AppService extends Service implements DataSendCallback, SensorEvent
         Intent dialIntent = new Intent(this, ContactActivity.class);
         dialIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(dialIntent);
-        uploadLog("设备开始报警...");
+//        uploadLog("设备开始报警...");
         ApiRequest.fall(fallType, new HttpSubscriber() {
             @Override
             public void onError(Throwable e) {
                 super.onError(e);
-                uploadLog("报警指令送达失败...");
+//                uploadLog("报警指令送达失败...");
             }
 
             @Override
             public void onNext(HttpResult result) {
                 super.onNext(result);
                 if (result.code.equals(AppConfig.SUCCESS)) {
-                    uploadLog("报警指令送达成功...");
+//                    uploadLog("报警指令送达成功...");
                 } else {
-                    uploadLog("报警指令送达失败...");
+//                    uploadLog("报警指令送达失败...");
                 }
             }
         });
